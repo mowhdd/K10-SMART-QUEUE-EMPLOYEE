3af5868d4ef8e8fbcd4e2742e2f63c740ce4acb7
Mohamed <bc255128@student.upm.edu.my>
2026-06-23 18:47:14 +0800

    test

--- customer.js
+++ customer.js
@@ -16,7 +16,7 @@ const menuItems = [
     price: "RM 12"
   },
   {
-    name: "Nasi Lemak Ayam Goreng",
+    name: "Nasi Lemak Ayam",
     category: "Rice",
     description: "Coconut rice, crispy fried chicken, sambal, egg, and peanuts.",
     price: "RM 14"
